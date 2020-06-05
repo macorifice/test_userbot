5c600fe264a3e263dcafd2aea74ca1bee1650cd1
Stefano Di Cecco <stefano.dicecco@fitprime.com>
2020-06-05 22:20:34 +0200

    [*] fix json & add next()

--- routes/index.js
+++ routes/index.js
@@ -7,41 +7,40 @@ router.get("/", function (req, res) {
   res.json({ message: "Welcome to userbot.ai!" });
 });
 
-router.get("/primi/:n", function (req, res) {
+router.get("/primi/:n", function (req, res, next) {
   const isAnInteger = Number.isInteger(parseInt(req.params.n));
   let response;
 
   if (req.params.n <= 0) {
-    res.send(
-      res.json({
-        statusCode: 500,
-        message: "Input is minor or equal than 0",
-      })
-    );
+    res.send({
+      statusCode: 500,
+      message: "Input is minor or equal than 0",
+    });
+    return next();
   }
 
   if (isAnInteger) {
     try {
       const resultPrimi = getNumeriPrimi(req.params.n);
-      response = res.json({
+      response = {
         statusCode: 200,
         message: "Result of primi : " + resultPrimi,
-      });
+      };
     } catch (e) {
-      response = res.json({
+      response = {
         statusCode: 500,
         message: "Error : " + e,
-      });
+      };
     }
 
     res.send(response);
+    return next();
   }
-  res.send(
-    res.json({
-      statusCode: 500,
-      message: "Input is not an integer",
-    })
-  );
+  res.send({
+    statusCode: 500,
+    message: "Input is not an integer",
+  });
+  return next();
 });
 
 module.exports.router = router;
