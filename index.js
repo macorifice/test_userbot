2cad3a089f1c8e8f2a74772057b2849258419e8a
Stefano Di Cecco <stefano.dicecco@fitprime.com>
2020-06-05 15:48:48 +0200

    [*] add gitignore optimize docker & add nodemon

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
+
 const express = require("express");
 const app = express();
 
-app.get("/", function (req, res) {
-  res.send("Welcome to test userbot.ai");
-});
-
-app.get("/primi", function (req, res) {
-  const resultPrimi = primi(req.query.n);
-  const response = "Result of primi : " + JSON.stringify(resultPrimi)
-  res.status(200).send(response)
-});
-
-const mod = (a, n) => {
-  return a - (n * Math.floor(a/n));
-}
-
 const numeri_primi = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
 
-const primi = (n) => {
+const getNumeriPrimi = (n) => {
   const result = [];
 
   for (let index = 0; index <= numeri_primi.length; index++) {
@@ -29,5 +16,14 @@ const primi = (n) => {
     
 };
 
+app.get("/", function (req, res) {
+  res.send("Welcome to test userbot.ai");
+});
+
+app.get("/primi", function (req, res) {
+  const resultPrimi = getNumeriPrimi(req.query.n);
+  const response = "Result of primi : " + JSON.stringify(resultPrimi)
+  res.status(200).send(response)
+});
 
 app.listen(3000);
