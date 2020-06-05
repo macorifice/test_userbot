55ac2bbb6066b5a8420a91baef7b84d4d66aab8d
Stefano Di Cecco <stefano.dicecco@fitprime.com>
2020-06-05 14:43:22 +0200

    [*] fix docker version & result

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,13 @@
-FROM node:7.7.2-alpine
-WORKDIR /usr/app
-COPY package.json .
-RUN npm install --quiet
-COPY . .
+FROM node:12-alpine
+  
+WORKDIR /node
+
+COPY package*.json ./
+
+RUN npm install && npm cache clean --force --loglevel=error
+
+WORKDIR /node/app
+
+COPY ./index.js index.js
+
+CMD [ "node", "index.js"]
--- server.js
+++ server.js
@@ -7,19 +7,27 @@ app.get("/", function (req, res) {
 
 app.get("/primi", function (req, res) {
   const resultPrimi = primi(req.query.n);
-  res.send("Result of primi : ", JSON.stringify(resultPrimi));
-  
+  const response = "Result of primi : " + JSON.stringify(resultPrimi)
+  res.status(200).send(response)
 });
+
+const mod = (a, n) => {
+  return a - (n * Math.floor(a/n));
+}
+
+const numeri_primi = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
 
 const primi = (n) => {
   const result = [];
-  for (const i = 0; i <= n; i++) {
-    if (n > 0 && n/n === 1) {
-      result.push(i);
-    }
 
-    return result;
-  }
+  for (let index = 0; index <= numeri_primi.length; index++) {
+    if (numeri_primi[index] < n)
+      result.push(numeri_primi[index]);
+    }
+    
+      return result;
+    
 };
+
 
 app.listen(3000);
